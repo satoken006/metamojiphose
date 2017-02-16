9a549bdb9f7b558e0e49d879ed1f5ebd857007cd
satoken912 <seat.soil.74196901@gmail.com>
2017-02-16 17:50:58 +0900

    hoge

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-
-
-function setup(){
-	createCanvas(500, 400);
-}
-
-function draw(){
-	ellipse(100, 100, 100, 100);
-	rect(300, 300, 50, 50);
-}
-
+var s;
 
 function Point( x, y ){
 	this.x = x;
 	this.y = y;
+}
+
+function Stroke(){
+	this.p_list = [];
+}
+
+
+function setup(){
+	createCanvas(500, 400);
+	s = new Stroke();
+	console.log( s );
+}
+
+
+function draw(){
+	ellipse(100, 100, 100, 100);
+	rect(300, 300, 50, 50);
+	/*
+	for( var i = 0 ; i < s.p_list.length ; i ++ ){
+		point( s.p_list[i].x, s.p_list[i].y );
+	}
+	*/
+}
+
+
+function mouseDragged(){
+	s.p_list.push( new Point(mouseX, mouseY ) );
+	console.log( new Point( mouseX, mouseY ) );
 }
