eccca3c403ad885c93845f285824548587fd6c23
satoken912 <seat.soil.74196901@gmail.com>
2017-02-16 18:12:10 +0900

    hoge

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
-var s;
+var char = []; // Strokeのリスト
+var new_stroke;
 
 function Point( x, y ){
 	this.x = x;
 	this.y = y;
 }
 
+
 function Stroke(){
 	this.p_list = [];
 }
 
+// *************************
 
 function setup(){
 	createCanvas(500, 400);
-	s = new Stroke();
-	console.log( s );
+	strokeWeight(3);
 }
 
 
 function draw(){
-	ellipse(100, 100, 100, 100);
-	rect(300, 300, 50, 50);
-	/*
-	for( var i = 0 ; i < s.p_list.length ; i ++ ){
-		point( s.p_list[i].x, s.p_list[i].y );
+	//ellipse(100, 100, 100, 100);
+	//rect(300, 300, 50, 50);
+	
+	for( var si = 0 ; si < char.length ; si ++ ){
+		for( var pi = 0 ; pi < char[si].p_list.length ; pi ++ ){
+			point( char[si].p_list[pi].x, char[si].p_list[pi].y );
+		}
 	}
-	*/
+	
+}
+
+
+function mousePressed(){
+	new_stroke = new Stroke();
 }
 
 
 function mouseDragged(){
-	s.p_list.push( new Point(mouseX, mouseY ) );
+	new_stroke.push( new Point( mouseX, mouseY ) );
 	console.log( new Point( mouseX, mouseY ) );
+}
+
+
+function mouseReleased(){
+	char.push( new_stroke );
 }
