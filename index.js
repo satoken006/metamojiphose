6fd8040a03fc062d6b2aa357ffd24613be203d1f
satoken912 <seat.soil.74196901@gmail.com>
2017-02-17 16:01:58 +0900

    add canvas to input

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var char_stroke = []; // Strokeのリスト
-var new_stroke;
-
-var THR_LENGTH = 20;
-var spline;
-
 function Point( x, y ){
 	this.x = x;
 	this.y = y;
@@ -14,49 +8,61 @@ function Stroke(){
 	this.p_list = [];
 }
 
-// *************************
+// ************************* 入力
 
-function setup(){
-	createCanvas(500, 400);
-	strokeWeight(2);
+var app_input = function(p){
 
-	new_stroke = [];
-	spline = new Spline();
-}
+	var char_stroke = []; // Strokeのリスト
+	var new_stroke;
+
+	var THR_LENGTH = 20;
+	var spline;
+
+	p.setup = function(){
+		p.createCanvas(300, 300);
+		//canvas_input.parent( "canvas_input" );
+		p.strokeWeight(2.5);
+
+		new_stroke = [];
+		spline = new Spline();
+	}
 
 
-function draw(){
-	background(255, 255, 255);
+	p.draw = function(){
+		p.background(255);
 
-	for( var si = 0 ; si < char_stroke.length ; si ++ ){
-		var s = char_stroke[si];
-		for( var pi = 0 ; pi < s.p_list.length ; pi ++ ){
-			point( s.p_list[pi].x, s.p_list[pi].y );
+		for( var si = 0 ; si < char_stroke.length ; si ++ ){
+			var s = char_stroke[si];
+			for( var pi = 0 ; pi < s.p_list.length ; pi ++ ){
+				p.point( s.p_list[pi].x, s.p_list[pi].y );
+			}
+		}
+	}
+
+
+	p.mousePressed = function(){
+		char_stroke.push( new Stroke() );
+	}
+
+
+	p.mouseDragged = function(){
+		var last = char_stroke.length-1;
+		char_stroke[last].p_list.push( new Point( p.mouseX, p.mouseY ) );
+	}
+
+
+	p.mouseReleased = function(){
+		var last = char_stroke.length-1;
+		var len_last = char_stroke[last].p_list.length;
+
+		if( len_last < THR_LENGTH ){
+			char_stroke.pop();
+		}else{
+			var list = char_stroke[last].p_list;
+			var list2 = spline.getSpline( list, 100 );
+			char_stroke[last].p_list = list2;
 		}
 	}
 }
 
-
-function mousePressed(){
-	char_stroke.push( new Stroke() );
-}
-
-
-function mouseDragged(){
-	var last = char_stroke.length-1;
-	char_stroke[last].p_list.push( new Point( mouseX, mouseY ) );
-}
-
-
-function mouseReleased(){
-	var last = char_stroke.length-1;
-	var len_last = char_stroke[last].p_list.length;
-
-	if( len_last < THR_LENGTH ){
-		char_stroke.pop();
-	}else{
-		var list = char_stroke[last].p_list;
-		var list2 = spline.getSpline( list, 2 );
-		char_stroke[last].p_list = list2;
-	}
-}
+new p5( app_input, "canvas_input" );
